137f0fcd662abe4e0d85027625afb0f200fa79a6
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-09 23:24:24 +0900

    1

---
+++ src/components/Post.vue
@@ -0,0 +1,83 @@
+<!-- 模擬データを一覧表示させる -->
+
+<template>
+  <v-app>
+      <v-main>
+          <div id='block'>
+              <v-row
+              style='width: 550px;'
+              >
+                <v-toolbar color='grey lighten-1'>
+                    <v-toolbar-title>本棚</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn color='primary' dark class='mb-1' @click='showDialogNew'>新規追加</v-btn>
+                </v-toolbar>
+
+                <!-- 本棚の中身-->
+                <div
+                v-for='post in posts' :key='post.id'
+                >
+                <v-card
+                    class='mx-auto'
+                    width='550px'
+                    outlined
+                >
+                <v-list-item three-line>
+                    <v-list-item-content>
+                        <v-list-item-title class='headline mb-1'>
+                            {{post.title}} / {{post.author}}
+                        </v-list-item-title>
+                        <v-list-item-subtitle>From Apollo-Server</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <!-- 変風・削除ボタン -->
+                <v-card-actions>
+                    <v-btn
+                        color='success'
+                        small
+                    >
+                    <v-icon small>
+                            編集する
+                    </v-icon>
+                    </v-btn>
+                    <v-btn
+                        color="error"
+                        small
+                    >
+                    <v-icon small>
+                        削除する
+                    </v-icon>
+                    </v-btn>
+                </v-card-actions>
+         </v-card>
+  </div>
+  </v-main>
+  </v-app>
+</template>
+
+<script>
+//Query
+import {ALL_POSTS} from '../graphql/query';
+export default {
+    name:'Post',
+    data:() => ({
+        //本棚の中身を定義
+        posts: [],
+    }),
+    apollo: {
+        //本棚の中身
+        posts: {
+            //クエリを書いている部分
+            query:ALL_POSTS,
+        },
+    },
+    methods: {},
+};
+</script>
+
+<style>
+#block{
+    text-align:center;
+    display: inline-block;
+}
+</style>
