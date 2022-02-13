61522b0d53cbde8407f57bed248f9403f7496789
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-14 00:09:17 +0900

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+//component
 import Home from '../views/Home.vue'
+import createUser from '../views/createUser.vue'
+import Login from '../views/Login.vue'
+import { component } from 'vue/types/umd'
 
 Vue.use(VueRouter)
-
+//ルート定義
 const routes = [
   {
     path: '/',
@@ -11,13 +16,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name:'createUser',
+    path:'/createUser',
+    component:createUser
+  },
+  {
+    name:'Login',
   }
+
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
