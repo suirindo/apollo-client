f56b0a2551a1c4c3b3d93c567cf6c111d1559bfb
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-15 22:50:36 +0900

    1

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,15 @@
-/*
-ログイン状態を管理するためにVuexを導入する。
-Vuex：Vueアプリケーションで使用できる状態管理ライブラリ
-各Vueコンポーネントから管理下にあるデータの取得と更新ができるため、
-アプリケーション全体でデータの管理がしやすくなる
-*/
-
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import user from './modules/user';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-    },
-    mutations: {
-    },
-    actions:{
-    },
-    //指定したモジュールファイルをまとめて、main.jsにエクスポートする
-    modules: {
-        user
-    }
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
 })
