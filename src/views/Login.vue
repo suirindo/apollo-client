510f56e6f720bf952f6a08216d18725038099651
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-15 21:37:50 +0900

    1

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <router-link to = '/'>トップページ</router-link>
+        <v-form ref = 'form' v-model = 'valid' lazy-validation>
+            <v-text-field
+                type = 'email'
+                v-model = 'email'
+                :rules = 'emailRules'
+                label = 'メールアドレス'
+                required
+            ></v-text-field>
+            <v-text-field
+                type = 'password'
+                v-model = 'password'
+                :rules = 'passwordRules'
+                label = 'パスワード'
+                required
+            ></v-text-field>
+            <!-- ログインボタン -->
+            <v-btn
+                :disabled='!valid'
+                @click='login'
+            >
+                ログイン
+            </v-btn>
+        </v-form>
+    </div>
+</template>
